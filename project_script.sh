d37bed5f2b467579ede6644b55a47ccb2bfd1081
tehu <tehu@free.fr>
2024-10-18 10:30:54 +0200

    generation name_info_log

--- project_script.sh
+++ project_script.sh
@@ -3,7 +3,9 @@
 # Variables
 
 user_actif=1
+name_info_log=$(date +% "info_<Cible>_%Y%m%d.txt")
 
+# exemple de résultat : génère name_info_log="info_<Cible>_20241018.txt"
 #
 
 # Target network testing function
@@ -30,11 +32,16 @@ addUser()
 	read -p "Nom de l'utilisateur à créer " user_target
 	# cnx ssh
 	ssh -T $targetUsername@$targetIp <<eof
-	sudo useradd -m -s /bin/bash -p $user_target $user_target >> ./info_temp.txt
+	sudo useradd -m -s /bin/bash -p $user_target $user_target >> ./$name_info_log
 eof
-	scp $targetUsername@$targetIp:/home/$user_target/info_temp.txt ~/Documents/
+	scp $targetUsername@$targetIp:/home/$user_target/$name_info_log ~/Documents/
+
+
+
 	echo commande réalisée
 	echo retour au menu
+
+
 }
 
 supprUser()
@@ -56,8 +63,11 @@ restartTarget()
 # MAIN START
 
 # Asking user to input target IP address & target username
-read -p "Quelle est l'adresse IP de la cible?" targetIp
-read -p "Quel est le nom de l'utilisateur cible?" targetUsername
+read -p "Quelle est l'adresse IP de la cible : " targetIp
+read -p "Quel est le nom de l'utilisateur cible : " targetUsername
+
+name_info_log=$(echo $name_info_log | sed "s/<Cible>/$targetUsername/")
+# exemple : info_<Cible>_20241018.txt  devient  info_user_20241018.txt
 
 sshTest
 
